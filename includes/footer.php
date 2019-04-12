847fd91e79c28ea7b81f689d652da1d899122bf2
joaolfp <joaolucasfp2001@gmail.com>
2019-04-11 22:47:42 -0300

    #FEATURE: I put the links of the web site of the profiles.

--- includes/footer.php
+++ includes/footer.php
@@ -3,9 +3,9 @@
         <div class="row mb-3">
             <div class="col-md-12 text-center">
                 <p>
-                    <a href="#" class="social-item"><span class="icon-twitter" title="twitter"></span></a>
-                    <a href="#" class="social-item"><span class="icon-linkedin2" title="Linkedin"></span></a>
-                    <a href="#" class="social-item"><span class="icon-github" title="GitHub"></span></a>
+                    <a href="https://twitter.com/@lucasfp2001" target="_blank" class="social-item"><span class="icon-twitter" title="twitter"></span></a>
+                    <a href="https://www.linkedin.com/in/joão-lucas-francisco-pereira-9b66a8175/" target="_blank" class="social-item"><span class="icon-linkedin2" title="Linkedin"></span></a>
+                    <a href="https://github.com/joaolfp" target="_blank" class="social-item"><span class="icon-github2" title="GitHub"></span></a>
                 </p>
             </div>
         </div>
